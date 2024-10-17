d4c9ebf2671d35a800b7c80cd39fdf89153da904
ademontel <53986476+ademontel@users.noreply.github.com>
2024-10-17 00:32:34 +0000

    Todavia falta embellecer lo visual

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	let [listaDeTareas, setListaDeTareas] = useState(["bañarse","limpiar","cocinar","aprender react"]);
+	const [nuevaTarea, setNuevaTarea] = useState("")
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container mt-5">
+			<h1 className="text-center mt-5">TO-DOs</h1>
+			<div className="col-6 mx-auto">
+			<input type="text" className="form-control" placeholder="¿Qué tarea quieres hacer?" 
+			value={nuevaTarea} onChange={(evento) => {
+					setNuevaTarea(evento.target.value)
+				}}
+				onKeyUp={(evento) =>{
+					if (evento.key == "Enter") {
+						setListaDeTareas([...listaDeTareas ,nuevaTarea]);
+						setNuevaTarea("")
+						
+					}
+					console.log(evento.key)
+				}}
+
+			/>
+			<ul>
+				{listaDeTareas.map((item,index) =>{
+					return(
+					<li key={index}>
+						{item}<i onClick={()=>{
+							const aux = listaDeTareas.filter((_task, ind) => {
+								return(ind != index)
+							})
+							setListaDeTareas(aux)
+						}} 
+						className="fa-solid fa-trash icono-oculto"></i>
+					</li>
+				)})}
+			</ul>
+			<span>
+				{listaDeTareas.length} items left
+			</span><br />
+			<span>
+				{(listaDeTareas.length == 0)?"No hay tareas, agrega una":""}
+			</span>
+			</div>
 		</div>
 	);
 };
